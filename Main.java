26a12326963c49deddd581a476e45797a21a3ad7
Abhijeetdorge <95493140+Abhijeetdorge@users.noreply.github.com>
2024-07-02 14:29:07 +0530

    initial commit
    adding for loop in program to print my name

--- Main.java
+++ Main.java
@@ -1,10 +1,14 @@
 import java.util.Date;
 
-public class Main{
-  
+public class Main {
+
     public static void main(String[] args) {
         System.out.println("Hello from jenkins");
         System.out.println(new Date());
         System.out.println("hello git");
+        System.out.println("Hello bro");
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Abhijeet Dorge");
+        }
     }
 }
